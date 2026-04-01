647b2efa76bd4a0ab31fbf19f19a3503ae1bafa5
Bhagya B <114055494+Bhagya-06@users.noreply.github.com>
2026-04-01 17:23:39 +0530

    Update CORS middleware to allow all origins

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.listen(PORT, () => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
